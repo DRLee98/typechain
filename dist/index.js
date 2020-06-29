b138e9f871f6f3ab720cdb5685c82e8953cbc9cf
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-06-29 21:43:29 +0900

    BOOM!

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, "2020", "", "hello", 20200626);
+let blockchain = [genesisBlock];
+//# sourceMappingURL=index.js.map
